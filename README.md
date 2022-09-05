c0c0ae33d0e6b5852f9e0eaec2c324084771be66
Mikołaj Spytek <mikolajspytek@gmail.com>
2022-09-05 07:00:02 +0000

    version 0.1.1

---
+++ README.md
@@ -0,0 +1,38 @@
+# survex: Explainable Machine Learning in Survival Analysis <img src="man/figures/survex.png" align="right" width="150px"/>
+
+[![R-CMD-check](https://github.com/ModelOriented/survex/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/ModelOriented/survex/actions/workflows/R-CMD-check.yaml)
+[![Codecov test coverage](https://codecov.io/gh/ModelOriented/survex/branch/main/graph/badge.svg)](https://app.codecov.io/gh/ModelOriented/survex?branch=main)
+
+
+## Overview 
+
+Survival analysis is a task dealing with time-to-event prediction. Aside from the well understood models, many others have recently emerged, however most of them lack interpretability as they are black-box models. Due to the unusual, functional type of prediction (either in the form of survival function or cumulative hazard function) standard model agnostic explanations cannot be applied directly.
+
+The `survex` package provides model agnostic explanations for survival models. If you're unfamiliar with model agnostic explanations, consider looking at the [Explanatory Model Analysis](https://ema.drwhy.ai/) e-book, as most of the methods included in this package are extensions of those described in the book for models with functional outputs. 
+
+The main function `explain()` creates a standardized wrapper for a model, which is further used for calculating predictions. If you work with models from `mlr3proba`, `censored`, `ranger`, `randomForestSRC` or `survival` packages, creating explainers is automated, most often you only need to supply the `model` parameter to the `explain()` function.
+
+However, an explainer can be created for **any** survival model, using the `explain_survival()` function by passing `model`, `data`, `y`, and `predict_survival_function` arguments.
+
+
+## Installation
+
+The package can be installed from github using `devtools::install_github()`:
+
+```
+devtools::install_github("https://github.com/ModelOriented/survex")
+```
+
+
+## Usage
+
+[![`survex` usage cheatsheet](man/figures/cheatsheet.png)](https://github.com/ModelOriented/survex/blob/main/misc/cheatsheet.pdf)
+
+
+## Related work
+
+- H. Ishwaran, U. B. Kogalur, E. H. Blackstone, M. S. Lauer. [Random survival forests](https://projecteuclid.org/journalArticle/Download?urlId=10.1214%2F08-AOAS169). Annals of Applied Statistics, 2008.
+- M. S. Kovalev, L. V. Utkin, E. M. Kasimov. [SurvLIME: A method for explaining machine learning survival models](https://doi.org/10.1016/j.knosys.2020.106164). Knowledge-Based Systems, 2020.
+- R. Sonabend, F. J. Király, A. Bender, B. Bischl, M. Lang. [mlr3proba: an R package for machine learning in survival analysis](https://doi.org/10.1093/bioinformatics/btab039). Bioinformatics, 2021.
+- E. Hvitfeldt, H. Frick. [censored: 'parsnip' Engines for Survival Models](https://github.com/tidymodels/censored). CRAN v0.1.0, 2022.
+- M. Krzyziński, M. Spytek, H. Baniecki, P. Biecek. [SurvSHAP(t): Time-dependent explanations of machine learning survival models](https://arxiv.org/abs/2208.11080). arXiv preprint arXiv:2208.11080, 2022.
---
+++ inst/doc/survex-usage.R
@@ -0,0 +1,84 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(survex)
+library(survival)
+
+set.seed(123)
+
+vet <- survival::veteran
+
+cph <- coxph(Surv(time, status)~., data = vet, model = TRUE, x = TRUE)
+cph_exp <- explain(cph)
+
+rsf <- randomForestSRC::rfsrc(Surv(time, status)~., data = vet)
+rsf_exp <- explain(rsf)
+
+## -----------------------------------------------------------------------------
+predict(cph_exp, veteran[1:2,], output_type="risk")
+predict(rsf_exp, veteran[1:2,], output_type="risk")
+
+predict(cph_exp, veteran[1:2,], output_type="survival", times=seq(1, 600, 100))
+predict(rsf_exp, veteran[1:2,], output_type="survival", times=seq(1, 600, 100))
+
+predict(cph_exp, veteran[1:2,], output_type="chf", times=seq(1, 600, 100))
+predict(rsf_exp, veteran[1:2,], output_type="chf", times=seq(1, 600, 100))
+
+## ----warning=FALSE------------------------------------------------------------
+mp_cph <- model_performance(cph_exp)
+mp_rsf <- model_performance(rsf_exp)
+
+plot(mp_cph, mp_rsf)
+
+## -----------------------------------------------------------------------------
+plot(mp_cph, mp_rsf, metrics_type="scalar")
+
+## -----------------------------------------------------------------------------
+model_parts_rsf <- model_parts(rsf_exp)
+model_parts_cph <- model_parts(cph_exp)
+
+plot(model_parts_cph,model_parts_rsf)
+
+## -----------------------------------------------------------------------------
+model_parts_rsf_auc <- model_parts(rsf_exp, loss_function=loss_one_minus_cd_auc, type="difference")
+model_parts_cph_auc <- model_parts(cph_exp, loss_function=loss_one_minus_cd_auc, type="difference")
+
+# NOTE: this may take a long time, so a progress bar is available. To enable it, use:
+# progressr::with_progress(model_parts(rsf_exp, loss_function=loss_one_minus_cd_auc, type="difference"))
+
+plot(model_parts_cph_auc,model_parts_rsf_auc)
+
+## ---- fig.height=18-----------------------------------------------------------
+model_profile_cph <- model_profile(cph_exp, categorical_variables=c("trt", "prior"))
+plot(model_profile_cph, facet_ncol = 1)
+
+## ---- fig.height=18-----------------------------------------------------------
+model_profile_rsf <- model_profile(rsf_exp, categorical_variables=c("trt", "prior"))
+plot(model_profile_rsf, facet_ncol = 1, numerical_plot_type = "contour")
+
+## -----------------------------------------------------------------------------
+predict_parts_cph_32 <- predict_parts(cph_exp, veteran[32,])
+predict_parts_rsf_32 <- predict_parts(rsf_exp, veteran[32,])
+plot(predict_parts_cph_32, predict_parts_rsf_32)
+predict_parts_cph_12 <- predict_parts(cph_exp, veteran[12,])
+predict_parts_rsf_12 <- predict_parts(rsf_exp, veteran[12,])
+plot(predict_parts_cph_12, predict_parts_rsf_12)
+
+## -----------------------------------------------------------------------------
+predict_parts_cph_12_lime <- predict_parts(cph_exp, veteran[12,], type="survlime")
+predict_parts_rsf_12_lime <- predict_parts(rsf_exp, veteran[12,], type="survlime")
+plot(predict_parts_cph_12_lime, type="local_importance")
+plot(predict_parts_rsf_12_lime, type="local_importance")
+
+## ---- fig.height=18-----------------------------------------------------------
+predict_profile_cph_32 <- predict_profile(cph_exp, veteran[32,], categorical_variables=c("trt", "prior"))
+plot(predict_profile_cph_32, facet_ncol=1)
+
+## ---- fig.height=18-----------------------------------------------------------
+predict_profile_rsf_32 <- predict_profile(rsf_exp, veteran[32,], categorical_variables=c("trt", "prior"))
+plot(predict_profile_rsf_32, facet_ncol=1)
+
